b22414953c8295781fea6617b4aa2c24f7784bd8
Aniket Wazarkar <aniket.wazarkar@ril.com>
2025-04-23 07:36:01 +0530

    Fix OpenGraph image for social sharing

--- src/components/head.js
+++ src/components/head.js
@@ -26,7 +26,7 @@ const Head = ({ metadata }) => (
       {metadata.title}
     </title>
     <link rel="shortcut icon" href={favicon} />
-    <link rel="canonical" href="http://yashitanamdeo.github.io/" />
+    <link rel="canonical" href={config.siteUrl} />
 
     <meta name="description" content={metadata.description} />
     <meta name="keywords" content={config.siteKeywords} />
@@ -36,21 +36,21 @@ const Head = ({ metadata }) => (
     <meta property="og:type" content="website" />
     <meta property="og:url" content={metadata.siteUrl} />
     <meta property="og:site_name" content={metadata.title} />
-    <meta property="og:image" content={`${config.siteUrl}${ogImage}`} />
-    <meta property="og:image:width" content="1200" />
-    <meta property="og:image:height" content="630" />
+    <meta property="og:image" content={`${config.siteUrl}/images/og.png`} />
+    <meta property="og:image:width" content="1867" />
+    <meta property="og:image:height" content="909" />
     <meta property="og:image:type" content="image/png" />
     <meta property="og:locale" content={config.siteLanguage} />
     <meta itemProp="name" content={metadata.title} />
     <meta itemProp="description" content={metadata.description} />
-    <meta itemProp="image" content={`${config.siteUrl}${ogImage}`} />
+    <meta itemProp="image" content={`${config.siteUrl}/images/og.png`} />
     <meta name="twitter:card" content="summary_large_image" />
     <meta name="twitter:url" content={metadata.siteUrl} />
     <meta name="twitter:site" content={config.twitterHandle} />
     <meta name="twitter:creator" content={config.twitterHandle} />
     <meta name="twitter:title" content={metadata.title} />
     <meta name="twitter:description" content={metadata.description} />
-    <meta name="twitter:image" content={`${config.siteUrl}${ogImage}`} />
+    <meta name="twitter:image" content={`${config.siteUrl}/images/og.png`} />
     <meta name="twitter:image:alt" content={metadata.title} />
 
     <link rel="apple-touch-icon" sizes="57x57" href={appleIcon57x57} />
